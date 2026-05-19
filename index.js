4e4a22deb47c5435b3c228d7a7cefa72cd716c44
CodeWithRichit <richittyagi@gmail.com>
2026-05-20 05:29:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("btn1").addEventListener("click",function(){
+    localStorage.setItem("tesType","16personalities");
+    window.location.href="quiz.html";
+})
+document.getElementById("btn1").addEventListener("click",function(){
+    localStorage.setItem("testType","big5")
+    window.location.href="quiz.html";
+})
