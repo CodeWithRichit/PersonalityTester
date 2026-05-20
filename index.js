6f67be0c112f3a37d9c72020c7e3b2cd64721a6d
CodeWithRichit <richittyagi@gmail.com>
2026-05-20 05:46:15 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.getElementById("btn1").addEventListener("click",function(){
-    localStorage.setItem("tesType","16personalities");
+    localStorage.setItem("testType","16personalities");
     window.location.href="quiz.html";
 })
-document.getElementById("btn1").addEventListener("click",function(){
+document.getElementById("btn2").addEventListener("click",function(){
     localStorage.setItem("testType","big5")
     window.location.href="quiz.html";
 })
